078864038d7753e773853eddcfa69f6c6df70630
Dhika TB <dhikatb@gmail.com>
2020-01-03 13:37:48 +0700

    update server

--- src/server/index.js
+++ src/server/index.js
@@ -12,32 +12,21 @@ app.use(express.static(buildPath));
 
 app.post("/api", (req, res) => {
   const loginAccount = account[req.body.email];
-  const random = Math.floor(Math.random() * 7) + 1;
-  let st = 500;
-  let toSend = {};
-  let msg = "";
+  let st = 422;
+  let toSend = { message: "email / password is incorrect" };
+  let msg = "email / password is incorrect";
 
-  if (random === 4) {
-    toSend = { message: "something error" };
-    msg = "something error";
-  } else if (loginAccount) {
+  if (loginAccount) {
     if (loginAccount.password === req.body.password) {
       if (loginAccount.deactivated) {
-        st = 400;
+        st = 401;
         toSend = { message: "your account has deactivated" };
         msg = "your account has deactivated";
       } else {
         st = 200;
         toSend = loginAccount;
       }
-    } else {
-      st = 400;
-      toSend = { message: "email / password is incorrect" };
-      msg = "email / password is incorrect";
     }
-  } else {
-    toSend = { message: "something error" };
-    msg = "something error";
   }
 
   res.statusMessage = msg;
